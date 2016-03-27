34c1dd39262f146127f572f345372b51c1322ad8
Luke <rt.luke.s@gmail.com>
2016-03-27 10:12:58 +1000

    Added OTAMigrationAttribute for plugin migrations

---
+++ API/Data/Dapper/OTAMigrationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+using System;
+
+namespace OTA.Data.Dapper
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class OTAMigrationAttribute : MigrationAttribute
+    {
+        public OTAMigrationAttribute(long version, Type plugin)
+            : this(version, TransactionBehavior.Default, plugin)
+        {
+        }
+
+        public OTAMigrationAttribute(long version, string description, Type plugin)
+            : this(version, TransactionBehavior.Default, description, plugin)
+        {
+        }
+
+        public OTAMigrationAttribute(long version, TransactionBehavior transactionBehavior, Type plugin)
+            : this(version, transactionBehavior, null, plugin)
+        {
+        }
+
+        public OTAMigrationAttribute(long version, TransactionBehavior transactionBehavior, string description, Type plugin)
+            : base(GetVersion(version, plugin), transactionBehavior, description)
+        {
+
+        }
+
+        private static long GetVersion(long version, Type plugin)
+        {
+
+            return plugin.FullName.GetHashCode() + version;
+        }
+    }
+}
