dfc953d3a71d669fe10eb6723023b7d315ad2e7d
Luke <rt.luke.s@gmail.com>
2016-07-01 00:44:29 +1000

    Halloween module

---
+++ OTAPI.Modifications/OTAPI.Modifications.Main.Halloween/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OTAPI.Modifications.Main.Halloween")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OTAPI.Modifications.Main.Halloween")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfddc9c5-61c5-4f6b-9e7b-df79a03ee398")]
