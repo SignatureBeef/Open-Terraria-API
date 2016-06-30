541d9cdcc0395c71f874c87e4a609cfa7fdaddb4
Luke <rt.luke.s@gmail.com>
2016-06-30 23:34:20 +1000

    Started moving modifications to modules

---
+++ OTAPI.Modifications/OTAPI.Modifications.Collision.PressurePlate/Hooks/Collision/PressurePlate.cs
@@ -0,0 +1,22 @@
+﻿namespace OTAPI.Core
+{
+    public static partial class Hooks
+    {
+        public static partial class Collision
+        {
+            #region Handlers
+            public delegate HookResult PressurePlateHandler
+            (
+                ref int x,
+                ref int y,
+                ref global::Terraria.Entity entity
+            );
+            #endregion
+
+            /// <summary>
+            /// Occurs when an entity triggers a pressure plate.
+            /// </summary>
+            public static PressurePlateHandler PressurePlate;
+        }
+    }
+}
