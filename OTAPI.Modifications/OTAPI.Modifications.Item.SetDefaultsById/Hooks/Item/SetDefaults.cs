6316f637c3e2896ceba79df1b9802f6f74e56d37
Luke <rt.luke.s@gmail.com>
2016-06-30 23:56:21 +1000

    Item.SetDefaults* modules

--- OTAPI.Modifications/OTAPI.Modifications.Item.SetDefaultsById/Hooks/Item/SetDefaults.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Item.SetDefaultsById/Hooks/Item/SetDefaults.cs
@@ -5,26 +5,9 @@
         public static partial class Item
         {
             #region Handlers
-            public delegate HookResult PreSetDefaultsByNameHandler(global::Terraria.Item item, ref string name);
-            public delegate void PostSetDefaultsByNameHandler(global::Terraria.Item item, ref string name);
-
             public delegate HookResult PreSetDefaultsByIdHandler(global::Terraria.Item item, ref int type, ref bool noMatCheck);
             public delegate void PostSetDefaultsByIdHandler(global::Terraria.Item item, ref int type, ref bool noMatCheck);
             #endregion
-
-            /// <summary>
-            /// Occurs at the start of the SetDefaults(string) method.
-            /// Arg 1:  The item instance
-            ///     2:  Item name
-            /// </summary>
-            public static PreSetDefaultsByNameHandler PreSetDefaultsByName;
-
-            /// <summary>
-            /// Occurs when the SetDefaults(string) method ends
-            /// Arg 1:  The item instance
-            ///     2:  Item name
-            /// </summary>
-            public static PostSetDefaultsByNameHandler PostSetDefaultsByName;
 
             /// <summary>
             /// Occurs at the start of the SetDefaults(int,bool) method
---
+++ OTAPI.Modifications/OTAPI.Modifications.Item.SetDefaultsByName/Hooks/Item/SetDefaults.cs
@@ -0,0 +1,27 @@
+﻿namespace OTAPI.Core
+{
+    public static partial class Hooks
+    {
+        public static partial class Item
+        {
+            #region Handlers
+            public delegate HookResult PreSetDefaultsByNameHandler(global::Terraria.Item item, ref string name);
+            public delegate void PostSetDefaultsByNameHandler(global::Terraria.Item item, ref string name);
+            #endregion
+
+            /// <summary>
+            /// Occurs at the start of the SetDefaults(string) method.
+            /// Arg 1:  The item instance
+            ///     2:  Item name
+            /// </summary>
+            public static PreSetDefaultsByNameHandler PreSetDefaultsByName;
+
+            /// <summary>
+            /// Occurs when the SetDefaults(string) method ends
+            /// Arg 1:  The item instance
+            ///     2:  Item name
+            /// </summary>
+            public static PostSetDefaultsByNameHandler PostSetDefaultsByName;
+        }
+    }
+}
