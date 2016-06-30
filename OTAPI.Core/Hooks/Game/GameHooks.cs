dd8b53f3ff76399362a709a2fe58041141ad40da
Luke <rt.luke.s@gmail.com>
2016-07-01 00:37:10 +1000

    GameInitialize module

--- OTAPI.Core/Hooks/Game/GameHooks.cs
+++ OTAPI.Core/Hooks/Game/GameHooks.cs
@@ -9,16 +9,6 @@
             #endregion
 
             /// <summary>
-            /// Occurs at the first point of call when the Initialze method is ran.
-            /// </summary>
-            public static HookHandler PreInitialize;
-
-            /// <summary>
-            /// Occurs at the end of the games initialize method.
-            /// </summary>
-            public static HookHandler PostInitialize;
-
-            /// <summary>
             /// Occurs at the first point of call when the game is running the update loop.
             /// </summary>
             public static UpdateHandler PreUpdate;
---
+++ OTAPI.Modifications/OTAPI.Modifications.Main.GameInitialize/Hooks/Game/GameHooks.cs
@@ -0,0 +1,18 @@
+﻿namespace OTAPI.Core
+{
+    public static partial class Hooks
+    {
+        public static partial class Game
+        {
+            /// <summary>
+            /// Occurs at the first point of call when the Initialze method is ran.
+            /// </summary>
+            public static HookHandler PreInitialize;
+
+            /// <summary>
+            /// Occurs at the end of the games initialize method.
+            /// </summary>
+            public static HookHandler PostInitialize;
+        }
+    }
+}
