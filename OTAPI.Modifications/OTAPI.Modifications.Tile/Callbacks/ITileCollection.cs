9d475238d9c0fc8a617d65efc55a96a902bcdb58
Luke <rt.luke.s@gmail.com>
2016-07-11 21:37:08 +1000

    Cleaned and commented tile mods as its working

---
+++ OTAPI.Modifications/OTAPI.Modifications.Tile/Callbacks/ITileCollection.cs
@@ -0,0 +1,18 @@
+﻿namespace OTAPI.Tile
+{
+	/// <summary>
+	/// Describes accessors to a terraria tile collection.
+	/// This class is injected into the terraria assembly to replace
+	/// all occurrences of the Terraria.Main.tile array.
+	/// </summary>
+	public interface ITileCollection
+	{
+		/// <summary>
+		/// Describes the get and set tile accessors that the vanilla server will use
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		Terraria.Tile this[int x, int y] { get; set; }
+	}
+}
--- OTAPI.Modifications/OTAPI.Modifications.Tile/Callbacks/TileCollection.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Tile/Callbacks/TileCollection.cs
@@ -1,24 +1,36 @@
 ﻿namespace OTAPI.Tile
 {
-	public interface ITileCollection
-	{
-		Terraria.Tile this[int x, int y] { get; set; }
-	}
-
+	/// <summary>
+	/// Replicates the default functionality of a normal array.
+	/// </summary>
 	public class TileCollection : ITileCollection
 	{
 		protected Terraria.Tile[,] _tiles;
 
+		/// <summary>
+		/// Replicates the default terraria tile array constructor
+		/// </summary>
 		internal TileCollection() : this(new Terraria.Tile[Terraria.Main.maxTilesX, Terraria.Main.maxTilesY])
 		{
 
 		}
 
+		/// <summary>
+		/// Initializes a new instance with the specified collection
+		/// as the underlying source.
+		/// </summary>
+		/// <param name="collection"></param>
 		protected TileCollection(Terraria.Tile[,] collection)
 		{
 			_tiles = collection;
 		}
 
+		/// <summary>
+		/// Describes the get and set tile accessors that the vanilla server will use
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
 		public virtual Terraria.Tile this[int x, int y]
 		{
 			get
