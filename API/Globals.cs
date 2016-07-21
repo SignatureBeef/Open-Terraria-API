ec3e7634fc193e8223e0054de33410244a7919b9
Luke <rt.luke.s@gmail.com>
2016-07-22 07:39:33 +1000

    Terraria 1.3.2.1

--- API/Globals.cs
+++ API/Globals.cs
@@ -38,8 +38,8 @@ namespace OTA
             get { return String.Format("{0}.{1}{2}", Version.Major, Version.Minor, PhaseToSuffix(Version.Phase)); }
         }
 
-        public const Int32 TerrariaRelease = 172;
-        public const String TerrariaVersion = "1.3.2";
+        public const Int32 TerrariaRelease = 173;
+        public const String TerrariaVersion = "1.3.2.1";
 
         private const String WorldDirectory = "Worlds";
         private const String PluginDirectory = "Plugins";
