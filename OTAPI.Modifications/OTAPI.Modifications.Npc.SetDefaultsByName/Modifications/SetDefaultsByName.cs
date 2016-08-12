eeb31d58f511339dbf41ab6a7bb483b9b005faf9
Luke <rt.luke.s@gmail.com>
2016-08-12 18:09:35 +1000

    Enabled more hooks in the client

--- OTAPI.Modifications/OTAPI.Modifications.Npc.SetDefaultsByName/Modifications/SetDefaultsByName.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Npc.SetDefaultsByName/Modifications/SetDefaultsByName.cs
@@ -8,7 +8,8 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.Npc
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.3.2.1, Culture=neutral, PublicKeyToken=null"
+			"TerrariaServer, Version=1.3.2.1, Culture=neutral, PublicKeyToken=null",
+			"Terraria, Version=1.3.2.1, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => "Hooking Npc.SetDefaults(string)...";
 		public override void Run()
