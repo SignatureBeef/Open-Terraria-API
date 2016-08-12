4cd2008eee885fa010505353048b9189adc4b526
Luke <rt.luke.s@gmail.com>
2016-08-12 23:48:24 +1000

    Projectile kill
    Additionaly projectile mods are now client+server

---
+++ OTAPI.Modifications/OTAPI.Modifications.Projectile.Kill/Hooks/Projectile/Kill.cs
@@ -0,0 +1,17 @@
+﻿namespace OTAPI
+{
+    public static partial class Hooks
+    {
+        public static partial class Projectile
+        {
+            #region Handlers
+            public delegate HookResult PreKillHandler(global::Terraria.Projectile projectile);
+            public delegate void PostKilledHandler(global::Terraria.Projectile projectile);
+            #endregion
+			
+            public static PreKillHandler PreKill;
+			
+            public static PostKilledHandler PostKilled;
+        }
+    }
+}
--- OTAPI.Modifications/OTAPI.Modifications.Projectile.Kill/Modifications/Kill.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Projectile.Kill/Modifications/Kill.cs
@@ -1,27 +1,23 @@
 ﻿using OTAPI.Patcher.Engine.Extensions;
 using OTAPI.Patcher.Engine.Modification;
-using System.Linq;
 
 namespace OTAPI.Patcher.Engine.Modifications.Hooks.Projectile
 {
-	public class AI : ModificationBase
+	public class Kill : ModificationBase
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
 			"TerrariaServer, Version=1.3.2.1, Culture=neutral, PublicKeyToken=null",
 			"Terraria, Version=1.3.2.1, Culture=neutral, PublicKeyToken=null"
-
 		};
-		public override string Description => "Hooking Projectile.AI()...";
+		public override string Description => "Hooking Projectile.Kill()...";
+
 		public override void Run()
 		{
-			var vanilla = SourceDefinition.Type("Terraria.Projectile").Methods.Single(
-				x => x.Name == "AI"
-				&& x.Parameters.Count() == 0
-			);
+			var vanilla = SourceDefinition.Type("Terraria.Projectile").Method("Kill");
 
-			var cbkBegin = ModificationDefinition.Type("OTAPI.Callbacks.Terraria.Projectile").Method("AIBegin", parameters: vanilla.Parameters);
-			var cbkEnd = ModificationDefinition.Type("OTAPI.Callbacks.Terraria.Projectile").Method("AIEnd", parameters: vanilla.Parameters);
+			var cbkBegin = this.Method(() => Callbacks.Terraria.Projectile.KillBegin(null));
+			var cbkEnd = this.Method(() => Callbacks.Terraria.Projectile.KillEnd(null));
 
 			vanilla.Wrap
 			(
