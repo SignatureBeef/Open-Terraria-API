b7bc88ea98527de767c6c5af10560e59ccbd08c6
Luke <rt.luke.s@gmail.com>
2016-11-29 08:15:18 +1000

    Full NAT removal

---
+++ OTAPI.Modifications/OTAPI.Modifications.Mono/Modifications/RemoveNatFromNetplay.cs
@@ -0,0 +1,48 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using OTAPI.Patcher.Engine.Extensions;
+using OTAPI.Patcher.Engine.Modification;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OTAPI.Modifications.Mono.Modifications
+{
+	public class RemoveNatFromNetplay : ModificationBase
+	{
+		public override IEnumerable<string> AssemblyTargets => new[]
+		{
+			"TerrariaServer, Version=1.3.4.3, Culture=neutral, PublicKeyToken=null"
+		};
+		public override string Description => "Removing NAT from Netplay";
+
+		public override void Run()
+		{
+			var netplay = this.Type<Terraria.Netplay>();
+
+			foreach (var method in new[] {
+				this.Method(() => Terraria.Netplay.closePort()),
+				netplay.Method("OpenPort")
+			})
+			{
+				method.Body.Instructions.Clear();
+				method.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Ret));
+				method.Body.Variables.Clear();
+				method.Body.ExceptionHandlers.Clear();
+			}
+
+			netplay.Fields.Remove(netplay.Field("mappings"));
+			netplay.Fields.Remove(netplay.Field("upnpnat"));
+
+			var typesToRemove = new List<TypeDefinition>();
+			this.SourceDefinition.MainModule.ForEachType(type =>
+			{
+				if (type.Namespace.StartsWith("NATUPNPLib"))
+				{
+					typesToRemove.Add(type);
+				}
+			});
+			foreach (var type in typesToRemove)
+				this.SourceDefinition.MainModule.Types.Remove(type);
+		}
+	}
+}
