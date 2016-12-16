31f687f2c7bb994c85696ec3c9662cb5188dfee7
Luke <rt.luke.s@gmail.com>
2016-12-16 21:57:58 +1000

    Player,Npc,Item update hooks

--- OTAPI.Modifications/OTAPI.Modifications.Item.Update/Modifications/Update.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Item.Update/Modifications/Update.cs
@@ -1,7 +1,8 @@
 ﻿using OTAPI.Patcher.Engine.Extensions;
 using OTAPI.Patcher.Engine.Modification;
+using System.Linq;
 
-namespace OTAPI.Patcher.Engine.Modifications.Hooks.Player
+namespace OTAPI.Patcher.Engine.Modifications.Hooks.Item
 {
 	public class Update : ModificationBase
 	{
@@ -10,18 +11,18 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.Player
 			"TerrariaServer, Version=1.3.4.4, Culture=neutral, PublicKeyToken=null",
 			"Terraria, Version=1.3.4.3, Culture=neutral, PublicKeyToken=null"
 		};
-		public override string Description => "Hooking Player.Update";
+		public override string Description => "Hooking Item.UpdateItem";
 		public override void Run()
 		{
-			var vanilla = this.Method(() => (new Terraria.Player()).Update(0));
+			var vanilla = this.Method(() => (new Terraria.Item()).UpdateItem(0));
 
 			int tmp = 0;
 			var cbkBegin = this.SourceDefinition.MainModule.Import(
-				this.Method(() => OTAPI.Callbacks.Terraria.Player.UpdateBegin(null, ref tmp))
+				this.Method(() => OTAPI.Callbacks.Terraria.Item.UpdateBegin(null, ref tmp))
 			);
 
 			var cbkEnd = this.SourceDefinition.MainModule.Import(
-				this.Method(() => OTAPI.Callbacks.Terraria.Player.UpdateEnd(null,   tmp))
+				this.Method(() => OTAPI.Callbacks.Terraria.Item.UpdateEnd(null, tmp))
 			);
 
 			vanilla.Wrap
--- OTAPI.Modifications/OTAPI.Modifications.Npc.Update/Modifications/Update.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Npc.Update/Modifications/Update.cs
@@ -1,7 +1,7 @@
 ﻿using OTAPI.Patcher.Engine.Extensions;
 using OTAPI.Patcher.Engine.Modification;
 
-namespace OTAPI.Patcher.Engine.Modifications.Hooks.Player
+namespace OTAPI.Patcher.Engine.Modifications.Hooks.Npc
 {
 	public class Update : ModificationBase
 	{
@@ -10,18 +10,18 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.Player
 			"TerrariaServer, Version=1.3.4.4, Culture=neutral, PublicKeyToken=null",
 			"Terraria, Version=1.3.4.3, Culture=neutral, PublicKeyToken=null"
 		};
-		public override string Description => "Hooking Player.Update";
+		public override string Description => "Hooking Npc.UpdateNPC";
 		public override void Run()
 		{
-			var vanilla = this.Method(() => (new Terraria.Player()).Update(0));
+			var vanilla = this.Method(() => (new Terraria.NPC()).UpdateNPC(0));
 
 			int tmp = 0;
 			var cbkBegin = this.SourceDefinition.MainModule.Import(
-				this.Method(() => OTAPI.Callbacks.Terraria.Player.UpdateBegin(null, ref tmp))
+				this.Method(() => OTAPI.Callbacks.Terraria.Npc.UpdateBegin(null, ref tmp))
 			);
 
 			var cbkEnd = this.SourceDefinition.MainModule.Import(
-				this.Method(() => OTAPI.Callbacks.Terraria.Player.UpdateEnd(null,   tmp))
+				this.Method(() => OTAPI.Callbacks.Terraria.Npc.UpdateEnd(null, tmp))
 			);
 
 			vanilla.Wrap
