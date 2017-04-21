01317ef50eca6df84f98889ef4a5f63cc33ce61b
Luke <rt.luke.s@gmail.com>
2017-04-22 02:11:07 +1000

    Mono based servers can now delete worlds again
    2.0.0.21

---
+++ OTAPI.Modifications/OTAPI.Modifications.Mono/Callbacks/IO.cs
@@ -0,0 +1,27 @@
+﻿namespace OTAPI.Callbacks.Terraria
+{
+	internal static partial class IO
+	{
+		private static bool IsMono()
+		{
+			return System.Type.GetType("Mono.Runtime") != null;
+		}
+
+		internal static bool DeleteFile(string path)
+		{
+			bool deleted = false;
+
+			if (IsMono())
+			{
+				System.IO.File.Delete(path);
+				deleted = true;
+			}
+			else
+			{
+				deleted = global::Terraria.Utilities.FileOperationAPIWrapper.MoveToRecycleBin(path);
+			}
+
+			return deleted;
+		}
+	}
+}
---
+++ OTAPI.Modifications/OTAPI.Modifications.Mono/Modifications/HandlePlatformIO.cs
@@ -0,0 +1,40 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using OTAPI.Patcher.Engine.Extensions;
+using OTAPI.Patcher.Engine.Modification;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OTAPI.Modifications.Mono.Modifications
+{
+	/// <summary>
+	/// Since we use the Windows TerrariaServer.exe we need to handle windows specific code
+	/// In this case we need to replace all calls to 'FileOperationAPIWrapper.MoveToRecycleBin'
+	/// and redirect them to our own callback that will handle mono+.net
+	/// </summary>
+	public class HandlePlatformIO : ModificationBase
+	{
+		public override IEnumerable<string> AssemblyTargets => new[]
+		{
+			"TerrariaServer, Version=1.3.5.1, Culture=neutral, PublicKeyToken=null"
+		};
+		public override string Description => "Handling platform dependent code";
+
+		public override void Run()
+		{
+			var find = this.Method(() => Terraria.Utilities.FileOperationAPIWrapper.MoveToRecycleBin(null));
+			var callback = find.Module.Import(
+				this.Method(() => OTAPI.Callbacks.Terraria.IO.DeleteFile(null))
+			);
+
+			this.SourceDefinition.MainModule.ForEachInstruction((method, ins) =>
+			{
+				var call_method = ins.Operand as MethodReference;
+				if (call_method != null && call_method.FullName == find.FullName)
+				{
+					ins.Operand = callback;
+				}
+			});
+		}
+	}
+}
