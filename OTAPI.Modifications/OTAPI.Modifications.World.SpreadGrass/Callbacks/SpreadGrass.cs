07b355c91f7bcd54a1335a86d2aedf1b5d083aee
Luke <rt.luke.s@gmail.com>
2017-05-16 20:24:17 +1000

    SpreadGrass hook

---
+++ OTAPI.Modifications/OTAPI.Modifications.World.SpreadGrass/Callbacks/SpreadGrass.cs
@@ -0,0 +1,12 @@
+﻿namespace OTAPI.Callbacks.Terraria
+{
+	internal static partial class World
+	{
+		internal static bool SpreadGrass(ref int i, ref int j, ref int dirt, ref int grass, ref bool repeat, ref byte color)
+		{
+			var res = Hooks.World.SpreadGrass?.Invoke(ref i, ref j, ref dirt, ref grass, ref repeat, ref color);
+			if (res.HasValue) return res.Value == HookResult.Continue;
+			return true;
+		}
+	}
+}
---
+++ OTAPI.Modifications/OTAPI.Modifications.World.SpreadGrass/Hooks/SpreadGrass.cs
@@ -0,0 +1,17 @@
+﻿namespace OTAPI
+{
+	public static partial class Hooks
+	{
+		public static partial class World
+		{
+			#region Handlers
+			public delegate HookResult SpreadGrassHandler(ref int i, ref int j, ref int dirt, ref int grass, ref bool repeat, ref byte color);
+			#endregion
+
+			/// <summary>
+			/// Occurs when the game is attempting to spread grass
+			/// </summary>
+			public static SpreadGrassHandler SpreadGrass;
+		}
+	}
+}
