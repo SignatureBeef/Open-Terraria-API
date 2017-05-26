45744b4dae18c299b66ba782a4db2c541b119e8b
death <rt.luke.s@gmail.com>
2017-05-27 09:26:36 +1000

    Added a platform modification
    In place in the core modification as its a critical modification for
    mods to use when running on platforms other than windows.

---
+++ OTAPI.Modifications/OTAPI.Modifications.Core/Modifications/PlatformModification.cs
@@ -0,0 +1,21 @@
+﻿using OTAPI.Patcher.Engine.Extensions;
+using OTAPI.Patcher.Engine.Modification;
+using System.Collections.Generic;
+
+namespace OTAPI.Modifications
+{
+	public class PlatformModification : ModificationBase
+	{
+		public override IEnumerable<string> AssemblyTargets => new[]
+		{
+			"TerrariaServer, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null"
+		};
+
+		public override string Description => "Modifying readonly flag on ReLogic.OS.Platform.Current";
+
+		public override void Run()
+		{
+			this.SourceDefinition.Type("ReLogic.OS.Platform").Field("Current").IsInitOnly = false;
+		}
+	}
+}
