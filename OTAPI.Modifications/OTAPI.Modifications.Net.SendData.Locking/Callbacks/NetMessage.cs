affda660af7fd7851ac2acd6fe171c10bb3f2e91
AxeelAnder <axeel@qq.com>
2018-10-25 00:10:35 +0800

    fixed send section bug

---
+++ OTAPI.Modifications/OTAPI.Modifications.Net.SendData.Locking/Callbacks/NetMessage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTAPI.Patcher.Engine.Modifications.Hooks.Net
+{
+	internal partial class NetMessage
+	{
+		internal static int CompressTileBlock(int xStart, int yStart, short width, short height, BinaryWriter writer, int bufferStart)
+		{
+			using (MemoryStream rawStream = new MemoryStream())
+			{
+				using (BinaryWriter binaryWriter = new BinaryWriter(rawStream))
+				{
+					binaryWriter.Write(xStart);
+					binaryWriter.Write(yStart);
+					binaryWriter.Write(width);
+					binaryWriter.Write(height);
+					Terraria.NetMessage.CompressTileBlock_Inner(binaryWriter, xStart, yStart, (int)width, (int)height);
+					rawStream.Position = 0L;
+
+					MemoryStream compressedStream = new MemoryStream();
+					using (DeflateStream deflateStream = new DeflateStream(compressedStream, CompressionMode.Compress, true))
+					{
+						rawStream.CopyTo(deflateStream);
+						deflateStream.Flush();
+						deflateStream.Close();
+						deflateStream.Dispose();
+					}
+
+					if (rawStream.Length <= compressedStream.Length)
+					{
+						rawStream.Position = 0L;
+						writer.Write((byte)0);
+						writer.Write(rawStream.GetBuffer());
+					}
+					else
+					{
+						compressedStream.Position = 0L;
+						writer.Write((byte)1);
+						writer.Write(compressedStream.GetBuffer());
+					}
+				}
+			}
+
+			return 0;
+		}
+	}
+}
