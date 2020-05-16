86733d0a75ad5ea8538dde7c79a9d3864adf3796
Patrikkk <patrikveres@hotmail.com>
2020-05-16 19:30:10 +0200

    Update version string in Modifications
    From 1.3.5.3 to 1.4.0.1 current.
    Updated prebuild.ps1 to look in the correct folder for the exe.

--- OTAPI.Modifications/OTAPI.Modifications.Net.NetworkText/Modifications/SendChatMessageToClientModification.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Net.NetworkText/Modifications/SendChatMessageToClientModification.cs
@@ -9,7 +9,7 @@ namespace OTAPI.Modifications.NetworkText.Modifications
 	{
 		public override IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null",
+			"TerrariaServer, Version=1.4.0.0, Culture=neutral, PublicKeyToken=null",
 		};
 
 		public override string Description => "Hooking Terraria.NetMessage.SendChatMessageToClient";
