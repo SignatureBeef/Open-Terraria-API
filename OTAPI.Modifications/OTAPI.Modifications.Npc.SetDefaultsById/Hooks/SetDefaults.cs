9bc882b45014c86af33929dd8baf611665b2108b
鱼鱼 <axeel@qq.com>
2020-05-17 14:40:23 +0800

    Fix npc SetDefaults hooks and ReplaceWriter patch

--- OTAPI.Modifications/OTAPI.Modifications.Npc.SetDefaultsById/Hooks/SetDefaults.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Npc.SetDefaultsById/Hooks/SetDefaults.cs
@@ -1,12 +1,14 @@
-﻿namespace OTAPI
+﻿﻿using Terraria;
+
+ namespace OTAPI
 {
 	public static partial class Hooks
 	{
 		public static partial class Npc
 		{
 			#region Handlers
-			public delegate HookResult PreSetDefaultsByIdHandler(global::Terraria.NPC npc, ref int type, ref float scaleOverride);
-			public delegate void PostSetDefaultsByIdHandler(global::Terraria.NPC npc, int type, float scaleOverride);
+			public delegate HookResult PreSetDefaultsByIdHandler(global::Terraria.NPC npc, ref int type, ref NPCSpawnParams spawnparams);
+			public delegate void PostSetDefaultsByIdHandler(global::Terraria.NPC npc, int type, NPCSpawnParams spawnparams);
 			#endregion
 
 			/// <summary>
