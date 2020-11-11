beb2809f7730fdf95ccc5d1fae9a2ce6e93d2b82
Patrikkk <patrikveres@hotmail.com>
2020-11-11 01:43:12 +0100

    Updating modifications target assembly to 1.4.1.2

--- OTAPI.Modifications/OTAPI.Modifications.Wiring.AnnouncementBox/Modifications/AnnouncementBox.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Wiring.AnnouncementBox/Modifications/AnnouncementBox.cs
@@ -12,7 +12,7 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.World
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.4.1.1, Culture=neutral, PublicKeyToken=null"
+			"TerrariaServer, Version=1.4.1.2, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => "Hooking Wiring.HitWireSingle(x,y)...";
 		public override void Run()
