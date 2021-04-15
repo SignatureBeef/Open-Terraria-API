7956f21482902710996b41012228b2307319ffff
Luke <rt.luke.s@gmail.com>
2021-04-15 13:07:09 +1000

    Create HookPlayerNameCollision.cs

---
+++ ModFramework.Modules/Development/HookPlayerNameCollision.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using ModFramework;
+using Mono.Cecil.Cil;
+using MonoMod;
+
+partial class Development
+{
+    [Modification(ModType.PostPatch, "Hooking player name collisions")]
+    static void HookPlayerNameCollision(MonoModder modder)
+	{
+		int tmp;
+		var csr = modder.GetILCursor(() => (new Terraria.MessageBuffer()).GetData(0, 0, out tmp));
+
+        var flag = csr.Body.Instructions
+            .Single(x => x.OpCode == OpCodes.Ldstr && x.Operand.Equals("Net.NameTooLong"))
+            .Previous(y => y.OpCode == OpCodes.Brfalse_S);
+
+        var player = flag.Next(x => x.OpCode == OpCodes.Ldloc_S);
+
+        csr.Goto(flag, MonoMod.Cil.MoveType.After);
+
+        csr.Emit(OpCodes.Ldloc_S, player.Operand as VariableDefinition);
+        csr.EmitDelegate<PlayerNameCollisionCallback>(OTAPI.Callbacks.MessageBuffer.NameCollision);
+        csr.Emit(OpCodes.Brfalse_S, flag.Operand as Instruction);
+    }
+}
+
+[MonoMod.MonoModIgnore]
+public delegate bool PlayerNameCollisionCallback(Terraria.Player player);
+
+namespace OTAPI
+{
+    public static partial class Hooks
+    {
+        public static partial class MessageBuffer
+        {
+            public delegate HookResult NameCollisionHandler(Terraria.Player player);
+            public static NameCollisionHandler NameCollision;
+        }
+    }
+}
+
+namespace OTAPI.Callbacks
+{
+    public static partial class MessageBuffer
+    {
+        public static bool NameCollision(Terraria.Player player)
+        {
+            return Hooks.MessageBuffer.NameCollision?.Invoke(player) != HookResult.Cancel;
+        }
+    }
+}
