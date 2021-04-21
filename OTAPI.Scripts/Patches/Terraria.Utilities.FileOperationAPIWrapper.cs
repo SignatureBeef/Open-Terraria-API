4ceeed4440fa3e9871e7fcab3afd05c03d32bf28
Luke <rt.luke.s@gmail.com>
2021-04-21 11:49:39 +1000

    Fix world deletes

---
+++ OTAPI.Scripts/Patches/Terraria.Utilities.FileOperationAPIWrapper.cs
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (C) 2020 DeathCradle
+
+This file is part of Open Terraria API v3 (OTAPI)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using System.IO;
+
+namespace Terraria.Utilities
+{
+    public static class patch_FileOperationAPIWrapper
+    {
+        public static bool MoveToRecycleBin(string path)
+        {
+            File.Delete(path);
+            return !File.Exists(path);
+        }
+    }
+}
