e63afc8081db87a316b68fdd06dfdbea91bebe88
Luke <rt.luke.s@gmail.com>
2021-04-21 17:16:59 +1000

    Socket and player announce hooks
    Added license to more of the code as well.

---
+++ OTAPI.Scripts/Modifications/HookClientSockets.cs
@@ -0,0 +1,71 @@
+﻿/*
+Copyright (C) 2020 DeathCradle
+
+This file is part of Open Terraria API v3 (OTAPI)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#if !tModLoaderServer_V1_3
+using ModFramework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using System;
+using Terraria.Net.Sockets;
+
+[Modification(ModType.PreMerge, "Hooking tcp interfaces")]
+void HookClientSockets(ModFwModder modder)
+{
+    var callback = modder.GetMethodDefinition(() => OTAPI.Callbacks.Netplay.CreateTcpListener());
+    var tcpSocket = modder.GetDefinition<TcpSocket>();
+    modder.OnRewritingMethodBody += (MonoModder modder, MethodBody body, Instruction instr, int instri) =>
+    {
+        if (instr.OpCode == OpCodes.Newobj
+            && instr.Operand is MethodReference mref
+            && mref.DeclaringType.FullName == tcpSocket.FullName
+            && body.Method.DeclaringType.FullName != tcpSocket.FullName
+            && !body.Method.DeclaringType.Namespace.StartsWith("OTAPI")
+        )
+        {
+            if (mref.Parameters.Count != 0)
+                throw new Exception($"Expected no parameters for {tcpSocket.FullName} in {body.Method.FullName}");
+
+            instr.OpCode = OpCodes.Call;
+            instr.Operand = callback;
+        }
+    };
+}
+
+namespace OTAPI.Callbacks
+{
+    public static partial class Netplay
+    {
+        public static ISocket CreateTcpListener()
+            => Hooks.Netplay.CreateTcpListener?.Invoke() ?? new TcpSocket();
+    }
+}
+
+namespace OTAPI
+{
+    public static partial class Hooks
+    {
+        public static partial class Netplay
+        {
+            public delegate ISocket CreateTcpListenerHandler();
+            public static CreateTcpListenerHandler CreateTcpListener;
+        }
+    }
+}
+
+#endif
---
+++ OTAPI.Scripts/Modifications/HookPlayerAnnouncements.cs
@@ -0,0 +1,69 @@
+﻿/*
+Copyright (C) 2020 DeathCradle
+
+This file is part of Open Terraria API v3 (OTAPI)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+using Microsoft.Xna.Framework;
+using ModFramework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using Terraria.Localization;
+
+[Modification(ModType.PreMerge, "Hooking player announements (has join, has left)")]
+void HookPlayerAnnouncements(ModFwModder modder)
+{
+    var syncOne = modder.GetReference(() => Terraria.NetMessage.SyncOnePlayer(0, 0, 0));
+    var broadcast = modder.GetReference(() => Terraria.Chat.ChatHelper.BroadcastChatMessage(null, default, 0));
+    var callback = modder.GetMethodDefinition(() => OTAPI.Callbacks.NetMessage.PlayerAnnounce(null, default, 0));
+
+    modder.OnRewritingMethodBody += (MonoModder modder, MethodBody body, Instruction instr, int instri) =>
+    {
+        if (instr.Operand is MethodReference mref
+            && mref.FullName == broadcast.FullName
+            && body.Method.FullName == syncOne.FullName
+        )
+        {
+            instr.Operand = callback;
+        }
+    };
+}
+
+namespace OTAPI.Callbacks
+{
+    public static partial class NetMessage
+    {
+        public static void PlayerAnnounce(NetworkText text, Color color, int excludedPlayer = -1)
+        {
+            var result = Hooks.NetMessage.PlayerAnnounce?.Invoke(text, color, excludedPlayer);
+
+            if (result != HookResult.Cancel)
+                Terraria.Chat.ChatHelper.BroadcastChatMessage(text, color, excludedPlayer);
+        }
+    }
+}
+
+namespace OTAPI
+{
+    public static partial class Hooks
+    {
+        public static partial class NetMessage
+        {
+            public delegate HookResult PlayerAnnounceHandler(NetworkText text, Color color, int excludedPlayer = -1);
+            public static PlayerAnnounceHandler PlayerAnnounce;
+        }
+    }
+}
