c58a0f29e09138cdf9c21f74984896b8e46ddad4
Moneylover3246 <66443019+Moneylover3246@users.noreply.github.com>
2021-07-13 23:23:22 -0400

    Add hook for `NetMessage.ReceiveBytes()`

---
+++ OTAPI.Modifications/OTAPI.Modifications.Net.ReceiveBytes/Callbacks/ReceiveBytes.cs
@@ -0,0 +1,14 @@
+﻿namespace OTAPI.Callbacks.Terraria
+{
+    internal static partial class NetMessage
+    {
+        internal static bool ReceiveBytes(ref byte[] bytes, ref int streamLength, ref int bufferIndex)
+        {
+            var result = Hooks.Net.ReceiveBytes?.Invoke(ref bytes, ref streamLength, ref bufferIndex);
+            if (result.HasValue && result.Value == HookResult.Cancel)
+                return false;
+
+            return true;
+        }
+    }
+}
