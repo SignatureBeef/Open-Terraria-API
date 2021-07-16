9bcbb57bee55f148d1ce61a231f2d52bf75ea19b
Luke <rt.luke.s@gmail.com>
2021-07-17 01:04:41 +1000

    Update App.axaml.cs
    CI trigger maybe

--- OTAPI.Client.Installer/App.axaml.cs
+++ OTAPI.Client.Installer/App.axaml.cs
@@ -14,12 +14,7 @@ namespace OTAPI.Client.Installer
         public override void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow()
-                {
-                    //DataContext = new MainWindowViewModel(),
-                };
-            }
+                desktop.MainWindow = new MainWindow();
 
             base.OnFrameworkInitializationCompleted();
         }
