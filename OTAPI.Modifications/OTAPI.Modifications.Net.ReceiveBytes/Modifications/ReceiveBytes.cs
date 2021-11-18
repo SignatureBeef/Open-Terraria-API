fabd917286e8d383253e429624b310535228ef2b
Patrikkk <patrikveres@hotmail.com>
2021-11-19 00:00:46 +0100

    Updating target modifications assembly to TerrariaServer 1.4.3.0

--- OTAPI.Modifications/OTAPI.Modifications.Net.ReceiveBytes/Modifications/ReceiveBytes.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Net.ReceiveBytes/Modifications/ReceiveBytes.cs
@@ -7,7 +7,7 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.Net
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.4.2.3, Culture=neutral, PublicKeyToken=null"
+			"TerrariaServer, Version=1.4.3.0, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => "Hooking NetMessage.ReceiveBytes...";
 
