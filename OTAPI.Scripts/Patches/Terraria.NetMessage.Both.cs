ddb6dcab8a7cd5323602c1b234f8f2a2c38ba00e
Luke <rt.luke.s@gmail.com>
2021-11-21 10:24:44 +1000

    3.0.21-alpha / 1.4.3.0

--- OTAPI.Scripts/Patches/Terraria.NetMessage.Both.cs
+++ OTAPI.Scripts/Patches/Terraria.NetMessage.Both.cs
@@ -31,9 +31,9 @@ namespace Terraria
 {
     partial class patch_NetMessage : Terraria.NetMessage
     {
-        public static extern void orig_SendData(int msgType, int remoteClient = -1, int ignoreClient = -1, NetworkText text = null, int number = 0, float number2 = 0f, float number3 = 0f, float number4 = 0f, int number5 = 0, int number6 = 0, int number7 = 0);
+        public static extern void orig_SendData(int msgType, int remoteClient = -1, int ignoreClient = -1, NetworkText text = null, int number = 0, float number2 = 0f, float number3 = 0f, float number4 = 0f, int number5 = 0, int number6 = 0, int number7 = 0, float number8 = 0);
 
-        public static void SendData(int msgType, int remoteClient = -1, int ignoreClient = -1, NetworkText text = null, int number = 0, float number2 = 0f, float number3 = 0f, float number4 = 0f, int number5 = 0, int number6 = 0, int number7 = 0)
+        public static void SendData(int msgType, int remoteClient = -1, int ignoreClient = -1, NetworkText text = null, int number = 0, float number2 = 0f, float number3 = 0f, float number4 = 0f, int number5 = 0, int number6 = 0, int number7 = 0, float number8 = 0)
         {
             var args = new Hooks.NetMessage.SendDataEventArgs()
             {
