e1a89d6661d8d7adaaeaffea7a8b727b6f7ff5a1
Luke <rt.luke.s@gmail.com>
2021-11-25 07:39:10 +1000

    1.4.3.2 tick

--- OTAPI.Modifications/OTAPI.Modifications.Collision.PressurePlate/Modifications/SwitchTiles.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Collision.PressurePlate/Modifications/SwitchTiles.cs
@@ -15,7 +15,7 @@ namespace OTAPI.Patcher.Engine.Modifications.Hooks.Collision
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.4.3.1, Culture=neutral, PublicKeyToken=null"
+			"TerrariaServer, Version=1.4.3.2, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => @"Hooking Collision.SwitchTiles\PressurePlate...";
 
--- OTAPI.Modifications/OTAPI.Modifications.Mono/Modifications/WindowService.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Mono/Modifications/WindowService.cs
@@ -8,7 +8,7 @@ namespace OTAPI.Modifications.Mono.Modifications
 	{
 		public override IEnumerable<string> AssemblyTargets => new[]
 		{
-			"TerrariaServer, Version=1.4.3.1, Culture=neutral, PublicKeyToken=null"
+			"TerrariaServer, Version=1.4.3.2, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => "Removing WindowService native calls...";
 
