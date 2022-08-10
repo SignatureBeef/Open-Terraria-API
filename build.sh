86d0f756fc76930ceca7f808bea8c98a49f085d1
Luke <rt.luke.s@gmail.com>
2022-08-10 21:03:49 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ cd ../../../../
 dotnet build OTAPI.Server.Launcher.sln
 cd OTAPI.Server.Launcher/bin/Debug/net6.0
 dotnet OTAPI.Server.Launcher.dll -test-init
-cd ../../../../../
+cd ../../../../
 cd OTAPI.Patcher/bin/Debug/net6.0
 dotnet run --project ../../../OTAPI.Patcher.csproj -patchTarget=m -latest=n --framework net6.0
 cd ../../../../
 dotnet build OTAPI.Server.Launcher.sln
 cd OTAPI.Server.Launcher/bin/Debug/net6.0
 dotnet OTAPI.Server.Launcher.dll -test-init
-cd ../../../../../
+cd ../../../../
 cd OTAPI.Patcher/bin/Debug/net6.0
 dotnet run --project ../../../OTAPI.Patcher.csproj -patchTarget=t -latest=n --framework net6.0
 cd ../../../../
 dotnet build OTAPI.Server.Launcher.sln
 cd OTAPI.Server.Launcher/bin/Debug/net6.0
 dotnet OTAPI.Server.Launcher.dll -test-init
-cd ../../../../../
+cd ../../../../
