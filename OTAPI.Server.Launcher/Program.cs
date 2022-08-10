3aaf6b415bdad3f2e7e23d2bca0c1b6a93ea4098
Luke <rt.luke.s@gmail.com>
2022-08-10 20:06:27 +1000

    Cleanup server launcher usings

--- OTAPI.Server.Launcher/Program.cs
+++ OTAPI.Server.Launcher/Program.cs
@@ -16,15 +16,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Loader;
-using Terraria.ModLoader;
+using MonoMod.RuntimeDetour;
 
 namespace OTAPI.Launcher
 {
