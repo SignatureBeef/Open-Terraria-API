10036db6af97f8c58a3371b2b4c0f2d1b4ddcaac
Janet Blackquill <uhhadd@gmail.com>
2022-09-29 16:41:21 +0000

    Update OTAPI.Modifications.Tile to 1.4.4.1

--- OTAPI.Modifications/OTAPI.Modifications.Tile/ITileCollection.cs
+++ OTAPI.Modifications/OTAPI.Modifications.Tile/ITileCollection.cs
@@ -38,7 +38,7 @@ namespace OTAPI.Tile
 
         byte liquid { get; set; }
 
-        short sTileHeader { get; set; }
+        ushort sTileHeader { get; set; }
 
         byte bTileHeader { get; set; }
 
@@ -77,12 +77,15 @@ namespace OTAPI.Tile
         void lava(bool lava);
         bool honey();
         void honey(bool honey);
+        bool shimmer();
+        void shimmer(bool shimmer);
         bool wire4();
         void wire4(bool wire4);
         int wallFrameX();
         void wallFrameX(int wallFrameX);
         byte frameNumber();
         void frameNumber(byte frameNumber);
+
         byte wallFrameNumber();
         void wallFrameNumber(byte wallFrameNumber);
         int wallFrameY();
@@ -91,6 +94,12 @@ namespace OTAPI.Tile
         void checkingLiquid(bool checkingLiquid);
         bool skipLiquid();
         void skipLiquid(bool skipLiquid);
+        bool invisibleBlock();
+        void invisibleBlock(bool invisibleBlock);
+        bool invisibleWall();
+        void invisibleWall(bool invisibleWall);
+        bool fullbrightBlock();
+        void fullbrightBlock(bool fullbrightBlock);
         byte color();
         void color(byte color);
         bool active();
@@ -109,76 +118,16 @@ namespace OTAPI.Tile
         void actuator(bool actuator);
         byte slope();
         void slope(byte slope);
+        bool fullbrightWall();
+        void fullbrightWall(bool fullbrightWall);
         void Clear(TileDataType types);
-
+        void CopyPaintAndCoating(ITile other);
+        Terraria.TileColorCache BlockColorAndCoating();
+        Terraria.TileColorCache WallColorAndCoating();
+        void UseBlockColors(Terraria.TileColorCache cache);
+        void UseWallColors(Terraria.TileColorCache cache);
+        void ClearBlockPaintAndCoating();
+        void ClearWallPaintAndCoating();
         string ToString();
-        ////int collisionType { get; }
-        ////ushort type { get; set; }
-        ////ushort wall { get; set; }
-        ////byte liquid { get; set; }
-        ////short sTileHeader { get; set; }
-        ////byte bTileHeader { get; set; }
-        ////byte bTileHeader2 { get; set; }
-        ////byte bTileHeader3 { get; set; }
-        ////short frameX { get; set; }
-        ////short frameY { get; set; }
-
-        //object Clone();
-        //void ClearEverything();
-        //void ClearTile();
-        //void CopyFrom(ITile from);
-        //bool isTheSameAs(ITile compTile);
-        //int blockType();
-        //void liquidType(int liquidType);
-        //byte liquidType();
-        //bool nactive();
-        //void ResetToType(ushort type);
-        //void ClearMetadata();
-        //Color actColor(Color oldColor);
-        //bool topSlope();
-        //bool bottomSlope();
-        //bool leftSlope();
-        //bool rightSlope();
-        //bool HasSameSlope(ITile tile);
-        //byte wallColor();
-        //void wallColor(byte wallColor);
-        //bool lava();
-        //void lava(bool lava);
-        //bool honey();
-        //void honey(bool honey);
-        //bool wire4();
-        //void wire4(bool wire4);
-        //int wallFrameX();
-        //void wallFrameX(int wallFrameX);
-        //byte frameNumber();
-        //void frameNumber(byte frameNumber);
-        //byte wallFrameNumber();
-        //void wallFrameNumber(byte wallFrameNumber);
-        //int wallFrameY();
-        //void wallFrameY(int wallFrameY);
-        //bool checkingLiquid();
-        //void checkingLiquid(bool checkingLiquid);
-        //bool skipLiquid();
-        //void skipLiquid(bool skipLiquid);
-        //byte color();
-        //void color(byte color);
-        //bool active();
-        //void active(bool active);
-        //bool inActive();
-        //void inActive(bool inActive);
-        //bool wire();
-        //void wire(bool wire);
-        //bool wire2();
-        //void wire2(bool wire2);
-        //bool wire3();
-        //void wire3(bool wire3);
-        //bool halfBrick();
-        //void halfBrick(bool halfBrick);
-        //bool actuator();
-        //void actuator(bool actuator);
-        //byte slope();
-        //void slope(byte slope);
-
-        //string ToString();
     }
 }
