1a8d5eb15f9e256797301c08b4bad3b2e63357c5
CCat <63068823+CedaryCat@users.noreply.github.com>
2026-02-24 20:06:09 +0800

    Make Damage_PVE_Inner patch branch explicit for 145+

--- OTAPI.Scripts/Patches/PatchNpcStrikeArgs.Server.cs
+++ OTAPI.Scripts/Patches/PatchNpcStrikeArgs.Server.cs
@@ -101,10 +101,11 @@ partial class NpcStrikeArgs
 
                             case "Player.ApplyDamageToNPC":
                             case "Player.ItemCheck_MeleeHitNPCs":
-                            case "Projectile.Damage":
                             case "Player.ProcessHitAgainstNPC":
                             case "NPC.StrikeNPC":
-                            case "Projectile.Damage_PVE_Inner":
+#if TerrariaServer_1450_OrAbove || Terraria__1450_OrAbove || tModLoader_1450_OrAbove
+                            case "Projectile.Damage_PVE_Inner": // introduced/split in 145+
+#endif
                                 body.GetILProcessor().InsertBefore(instr,
                                     new { OpCodes.Ldarg_0 }
                                 );
